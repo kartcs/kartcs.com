86e3a7f469636aaa30db8e509604cc06e4bdda86
kart <66599878+kartcs@users.noreply.github.com>
2025-08-19 00:52:13 -0500

    a good version methinks
    just a bit missing

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Fragment } from 'react';
 import ReactDOM from 'react-dom/client'
 import './App.css'
 
-function App() {
+function Loading() {
   return (
-    <div>
-      {/* beautiful button idk */}
-      <div className='flex justify-center items-center h-screen w-screen fixed'>
-        <button className='text-white border-2 bg-red-500 pl-5 pr-5 p-2 rounded-full flex justify-center items-center hover:bg-white hover:text-red-500 transition duration-100'>KILL KART....!</button>
-      </div>
-      {/* beautiful top bar thing idk what to call it man */}
-      <div className="flex items-center fixed">
-        <div className="bg-gray-500 flex flex-row h-2/12 w-fit gap-5 items-center rounded-xl m-2">
-          <img src='/kartBrand.png' alt='Drawing of an orange and white square cat face' className='flex h-15 w-15 hover:blur-xs transition duration-300' />
-          {/* <button className='rounded-xl h- bg-amber-950 text-white flex items-center justify-center text-sm'>diddy</button> */}
-        </div>
+    <div className='flex justify-center items-center h-screen w-screen fixed bg-ctp-mantle'>
+      <div className='flex flex-row justify-center items-center w-40 h-10 bg-ctp-surface0 rounded-full gap-4'>
+        <img src='/loadingCircle.png' className='animate-spin size-7'/>
+        <h1 className='text-ctp-peach text-lg'>LOADING...</h1>
       </div>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <div>
+        {/* center of screen div */}
+        <div className='flex flex-col justify-center items-center h-screen w-screen fixed gap-5'>
+          <div className='flex flex-col bg-ctp-base w-fit h-fit p-5 rounded-2xl gap-5'>
+            <h1 className='text-ctp-text text-center text-5xl'>Yo.... waddup...</h1>
+            <h1 className='text-ctp-subtext0 text-center text-sm'>welcome to the kart site thing</h1>
+          </div>
+          <div className='flex flex-col bg-ctp-base w-fit h-fit p-5 rounded-2xl gap-2'>
+            <h1 className='text-ctp-text text-center text-3xl'>here are various cool things i may have done</h1>
+            <h1 className='text-ctp-subtext0 text-center text-sm'>im not too sure either</h1>
+            <div className='grid grid-cols-2 grid-rows-2 gap-5]'>
+            </div>
+          </div>
+        </div>
+        {/* top bar thing area */}
+        <div className="flex items-center justify-center sticky">
+          <div className="bg-ctp-surface0 flex flex-row h-15 w-fit gap-5 pr-2 pl-2 items-center rounded-full m-2">
+            <button className='text-ctp-peach border-2 bg-ctp-surface0 pl-5 pr-5 p-2 rounded-full flex justify-center items-center hover:bg-ctp-mantle transition duration-250'>cool stuff</button>
+            <img src='/kartBrand.png' alt='Drawing of an orange and white square cat face' className='flex h-15 w-15' />
+            <button className='text-ctp-peach border-2 bg-ctp-surface0 pl-5 pr-5 p-2 rounded-full flex justify-center items-center hover:bg-ctp-mantle transition duration-250'>vidja games</button>
+          </div>
+        </div>
+      </div>
+    </Suspense>
   );
 }
 
