8eecb7c8f36ee76c9557b0e0b41915a4be22895c
kart <66599878+kartcs@users.noreply.github.com>
2025-08-18 20:11:50 -0500

    winning finally
    w mans!

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,19 @@ import './App.css'
 
 function App() {
   return (
-    <p className='text-white'>kms</p>
+    <div>
+      {/* beautiful button idk */}
+      <div className='flex justify-center items-center h-screen w-screen fixed'>
+        <button className='text-white border-2 bg-red-500 pl-5 pr-5 p-2 rounded-full flex justify-center items-center hover:bg-white hover:text-red-500 transition duration-100'>KILL KART....!</button>
+      </div>
+      {/* beautiful top bar thing idk what to call it man */}
+      <div className="flex items-center fixed">
+        <div className="bg-gray-500 flex flex-row h-2/12 w-fit gap-5 items-center rounded-xl m-2">
+          <img src='/kartBrand.png' alt='Drawing of an orange and white square cat face' className='flex h-15 w-15 hover:blur-xs transition duration-300' />
+          {/* <button className='rounded-xl h- bg-amber-950 text-white flex items-center justify-center text-sm'>diddy</button> */}
+        </div>
+      </div>
+    </div>
   );
 }
 
