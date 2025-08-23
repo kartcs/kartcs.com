615d4cc21316d7c6300a92ebebdbfa80e6085b6a
kart <66599878+kartcs@users.noreply.github.com>
2025-08-23 16:12:25 -0500

    work is in fact in progress

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import React, { Suspense, useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 
+import { 
+	ArrowDownOnSquareIcon,
+	CodeBracketIcon
+ } from '@heroicons/react/16/solid'
+
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
 function Loading() {
@@ -59,19 +64,17 @@ function Root() {
 							: 'animate-[fadeOut_0.5s_ease-in-out_forwards] pointer-events-none'
 					}`}
 				>
-					<div className="visible bg-ctp-base w-fit min-w-100 max-w-200 min-h-50 max-h-1080 p-5 h-fit flex flex-col rounded-2xl items-center">
+					<div className="visible bg-ctp-base w-fit min-h-50 max-h-1/2 max-w-1/2 p-5 h-fit flex flex-col rounded-2xl items-center">
 						<div className="invisible h-fit basis-20 w-relative">
 							<h1 className="visible text-ctp-text text-6xl text-center">
 								{popupSub}
 							</h1>
 						</div>
-						<br />
 						<div className="invisible h-fit basis-10 w-relative">
 							<h1 className="visible text-ctp-subtext0 text-2xl text-center">
 								{popupBody}
 							</h1>
 						</div>
-						<br />
 						<div className="invisible h-fit basis-20 w-relative flex justify-center items-center">
 							<button
 								className="visible text-center text-ctp-text border-2 bg-ctp-surface0 pl-5 pr-5 p-2 rounded-full hover:bg-ctp-surface1 transition duration-250 cursor-pointer"
@@ -111,7 +114,7 @@ function Root() {
 	const pages: { [key: string]: React.ReactNode } = {
 		home: (
 			<div className="flex justify-center items-center w-full h-screen flex-col">
-				<div className="flex flex-col bg-ctp-mantle w-fit h-fit p-5 rounded-2xl gap-5 items-center">
+				<div className="flex flex-col bg-ctp-mantle w-fit h-fit max-h-1/2 max-w-1/2 p-5 rounded-2xl gap-5 items-center">
 					<h1 className="text-ctp-text text-center text-4xl">
 						hello bro
 					</h1>
@@ -124,7 +127,7 @@ function Root() {
 						onClick={() =>
 							CreatePopup(
 								'great question',
-								`hover at the top of the screen to show the toolbar!! thats about it!!`,
+								`hover/tap at the top of the screen to show the toolbar!! thats about it!!`,
 								'ok thanks'
 							)
 						}
@@ -136,8 +139,32 @@ function Root() {
 			</div>
 		),
 		projects: (
-			<div className="flex justify-center items-center w-full h-screen flex-col">
-				<h1>s4mi is awesome</h1>
+			<div className="flex justify-center items-center w-full h-screen flex-col fixed pointer-events-none">
+				<div className='pointer-events-auto flex-col w-150 h-fit min-h-100 max-h-70% rounded-2xl bg-ctp-mantle overflow-scroll items-center p-5 gap-5'>
+					<div className='flex rounded-2x1 w-140 h-20 bg-ctp-crust p-2 flex-row'>
+						
+						<div className='flex-col text-lg text-ctp-text'>
+							<h1>
+								project name
+							</h1>
+							<h1 className='text-sm text-ctp-subtext0'>
+								this is a really cool project description that really describes this project and everything important about it that you would really want to know!
+							</h1>
+						</div>
+					</div>
+					<div className='w-140 h-20 bg-ctp-crust'>
+						hellos
+					</div>
+					<div className='w-140 h-20 bg-ctp-crust'>
+						hellos
+					</div>
+					<div className='w-140 h-20 bg-ctp-crust'>
+						hellos
+					</div>
+					<div className='w-140 h-20 bg-ctp-crust'>
+						hellos
+					</div>
+				</div>
 			</div>
 		),
 	};
@@ -145,7 +172,7 @@ function Root() {
 	return (
 		<Suspense fallback={<Loading />}>
 			<div className="text-ctp-text">
-				<div className="group w-full fixed overflow-hidden h-20">
+				<div className="group w-full fixed overflow-hidden h-1/12">
 					<div className="w-full transform translate-y-[-5rem] transition-all duration-400 ease-in-out group-hover:translate-y-0">
 						<div className="bar flex items-center justify-center">
 							<div className="bg-ctp-surface0 flex flex-row h-15 w-fit gap-5 pr-2 pl-2 items-center rounded-full m-2">
